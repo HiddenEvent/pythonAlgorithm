f2e54fd03b1445383e7246d166230d84733be764
RichardKim <hiddeneventor@gmail.com>
2020-04-27 23:53:17 +0900

    연쇄 행렬 곱셈 추가

--- 동적프로그래밍/MatrixMux.py
+++ 동적프로그래밍/MatrixMux.py
@@ -1,26 +1,25 @@
 # 연쇄 행렬 곱셈
 
-## 행렬의 갯수 별 경우의 수 구하기
+## 행렬의 갯수 별 경우의 수 구하기 => (n-1)의 팩토리얼
 # 3개의 행렬 => 2가지
 # 4개의 행렬 => 처음 3가지 * 2가지
 # 5개의 행렬 => 4 * 3 * 2
 # n개의 행렬 => (n-1)!
 # 결론 적으로 경우의 수는(n-1)의 팩토리얼 개이다.
 import sys
+N = 4
+d = [10, 30, 5, 60]
+M = [[0 for x in range(N)] for y in range(N)]
 
-R = [500] # Row
-C = [500] # Colum
-s = [[5,3], [3,2], [2,6]]
-def MatrixMux(x, y):
-    print(y)
-    if y - x <= 0 : return 0;
-    mm = sys.maxsize # 인트의 최대값
-    for k in range(x, y-1):
-        mm = min(mm, MatrixMux(x,k) + MatrixMux(k+1,y) + R[x]*C[k]*C[y])
-    return mm
+for diag in range(1, N):
+    for i in range(1, N - diag):
+        j = i + diag
+        M[i][j] = sys.maxsize
+        for k in range(i, j):
+            M[i][j] = min(M[i][j],
+                          M[i][k] + M[k + 1][j] + d[i - 1] * d[k] * d[j])
 
-result = MatrixMux(0, 10)
-print(result)
+print(M[1][N-1])
 
 
 
